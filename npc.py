530189b24307e1f96d3da2f89609228d8605f8b2
Incredible-Seilaam <natalkarutova@seznam.cz>
2024-04-10 13:40:12 +0200

    init commit

---
+++ npc.py
@@ -0,0 +1,11 @@
+import pygame
+
+class NPC(pygame.sprite.Sprite):
+    def __init__(self, position, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('graphics/player/down/down_0.png').convert_alpha()  # Example surface size, replace with your NPC image
+        self.rect = self.image.get_rect(topleft=position)
+        self.hitbox = self.rect.inflate(-20,-40)
+
+        #self.npc_sprites = pygame.sprite.Group()
+

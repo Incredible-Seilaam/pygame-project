15a9a9b0f58e3ba5db20dcf02702baee7233fa79
Incredible-Seilaam <natalkarutova@seznam.cz>
2024-04-19 20:39:59 +0200

    textura, chybi rename

--- npc.py
+++ npc.py
@@ -3,7 +3,7 @@ import pygame
 class NPC(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('graphics/player/down/down_0.png').convert_alpha()  # Example surface size, replace with your NPC image
+        self.image = pygame.image.load('graphics/player/down_idle/idle_down.png').convert_alpha()  # Example surface size, replace with your NPC image
         self.rect = self.image.get_rect(topleft=position)
         self.hitbox = self.rect.inflate(-20,-40)
 

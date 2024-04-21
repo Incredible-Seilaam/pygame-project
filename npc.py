ebf84be954c7b966463882c3821e93ad40b4f864
Incredible-Seilaam <natalkarutova@seznam.cz>
2024-04-21 12:21:38 +0200

    npc branch

--- npc.py
+++ npc.py
@@ -3,7 +3,7 @@ import pygame
 class NPC(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('graphics/npc/npc_idle.png').convert_alpha()  # Example surface size, replace with your NPC image
+        self.image = pygame.image.load('graphics/player/down/down_0.png').convert_alpha()  # Example surface size, replace with your NPC image
         self.rect = self.image.get_rect(topleft=position)
         self.hitbox = self.rect.inflate(-20,-40)
 
